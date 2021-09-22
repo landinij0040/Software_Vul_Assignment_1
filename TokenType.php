538fb1b0c0ade7ab5a26b5634bd1b521d094e978
landinij0040 <ijlandin@miners.utep.edu>
2021-09-22 12:58:09 -0600

    Added the enum "TokenType"

---
+++ TokenType.php
@@ -0,0 +1,17 @@
+<?php
+$TokenType = [
+    'LBRACKET' => 0,
+    'RBRACKET'=> 1,
+    'LSQUAREBRACKET' => 2,
+    'RSQUAREBRACKET' => 3,
+    'STRING' => 4,
+    'EQUAL' => 5,
+    'LESS' => 6,
+    'GREATER' => 7,
+    'IF' => 8,
+    'ELSE' => 9,
+    'ID' => 10,
+    'INT' => 11,
+    'EOF' =>12,
+    'OTHER' => 13
+];
